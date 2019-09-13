b7ee84c3a95616fb7f356962d1b0f9c193078a48
SnehPatel <sneh0107@gmail.com>
2019-09-13 10:39:34 -0400

    Added keyboard functionality

--- W2_IPad/W2_IPad/W2_IPad/ViewController.swift
+++ W2_IPad/W2_IPad/W2_IPad/ViewController.swift
@@ -9,18 +9,46 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate {
+    
+    @IBOutlet var tfName : UITextField!
+    @IBOutlet var tfEmail : UITextField!
+    
+    @IBOutlet var lbName : UILabel!
+    @IBOutlet var lbEmail : UILabel!
     
     @IBOutlet var webView : WKWebView!
     @IBOutlet var activity : UIActivityIndicatorView!
-
+    
+    // Used to close keyboard after completing input
+    // Connect from storyboard > select textView > drag delegate to yellow circle
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+    
+    // Used for the loading icon, start animating
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        activity.isHidden = false
+        activity.startAnimating()
+    }
+    
+    // Stop animating loading icon
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activity.isHidden = true
+        activity.stopAnimating()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let urlAddress = URL(string: "http://www.cnn.com")
+        let urlAddress = URL(string: "http://www.bluejays.com")
         let url = URLRequest(url: urlAddress!)
         webView.load(url)
+        
+        // self = this (java)
+        webView.navigationDelegate = self
+        
         
     }
 
