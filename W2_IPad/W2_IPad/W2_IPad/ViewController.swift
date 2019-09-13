ed613ff565e2ffde7979632ae2e657e298341aac
SnehPatel <sneh0107@gmail.com>
2019-09-13 09:57:36 -0400

    IPad project created

---
+++ W2_IPad/W2_IPad/W2_IPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  W2_IPad
+//
+//  Created by Sneh Patel on 2019-09-13.
+//  Copyright © 2019 Sneh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
