1193ee09257ea5aff14e9a1db36036598b243b36
SnehPatel <sneh0107@gmail.com>
2019-09-16 13:17:53 -0400

    Removed images + added Segues

--- W2_IPad/W3_Animations/W3_Animations/ViewControllers/ViewController.swift
+++ W2_IPad/W3_Animations/W3_Animations/ViewControllers/ViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Step 1 for setting up a segue
+    @IBAction func unwindToHomeVC(sender : UIStoryboardSegue){
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
