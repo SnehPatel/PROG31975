8709df13c2dc40f155482aaa53807084e2775c19
SnehPatel <sneh0107@gmail.com>
2019-09-16 13:35:33 -0400

    Implemented sound

--- W2_IPad/W3_Animations/W3_Animations/ViewControllers/SpinViewController.swift
+++ W2_IPad/W3_Animations/W3_Animations/ViewControllers/SpinViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
+// Import Av Foundation framework
+import AVFoundation
+
 class SpinViewController: UIViewController {
+    
+    @IBOutlet var volSlider : UISlider!
+    var soundPlayer : AVAudioPlayer?
+    
+    @IBAction func volumeDidChange(sender : UISlider){
+        soundPlayer?.volume = volSlider.value
+    }
+    
+    // Executes before viewDidLoad, to start playing music
+    override func viewWillAppear(_ animated: Bool) {
+        let soundURL = Bundle.main.path(forResource: "song1", ofType: "mp3")
+        let url = URL.init(fileURLWithPath: soundURL!)
+        soundPlayer = try! AVAudioPlayer.init(contentsOf: url)
+        
+        // Set starting time of MP3
+        soundPlayer?.currentTime = 0
+        // Set Default Volume
+        soundPlayer?.volume = volSlider.value
+        // -1 = loop sound file
+        soundPlayer?.numberOfLoops = -1
+        // Play sound
+        soundPlayer?.play()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
