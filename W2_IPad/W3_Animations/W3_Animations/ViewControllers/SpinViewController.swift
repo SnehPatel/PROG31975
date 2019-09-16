b5670c427c4a8199c4ba908eec945189e9ef68f1
SnehPatel <sneh0107@gmail.com>
2019-09-16 13:07:17 -0400

    Added ViewControllers

---
+++ W2_IPad/W3_Animations/W3_Animations/ViewControllers/SpinViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SpinViewController.swift
+//  W3_Animations
+//
+//  Created by Sneh Patel on 2019-09-16.
+//  Copyright © 2019 Sneh Patel. All rights reserved.
+//
+
+import UIKit
+
+class SpinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
