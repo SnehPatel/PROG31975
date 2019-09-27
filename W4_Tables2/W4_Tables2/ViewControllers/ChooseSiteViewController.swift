0a47dec26d9fb12dbe306729a487dd027dc9df13
SnehPatel <sneh0107@gmail.com>
2019-09-27 08:31:06 -0400

    W4_Tables2 created

---
+++ W4_Tables2/W4_Tables2/ViewControllers/ChooseSiteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChooseSiteViewController.swift
+//  W4_Tables2
+//
+//  Created by Sneh Patel on 2019-09-27.
+//  Copyright © 2019 Sneh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ChooseSiteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ W4_Tables2/W4_Tables2/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  W4_Tables2
+//
+//  Created by Sneh Patel on 2019-09-27.
+//  Copyright © 2019 Sneh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
