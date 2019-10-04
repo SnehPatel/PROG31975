30df7464519c0a36d900035e6057fe2240ba70ef
SnehPatel <sneh0107@gmail.com>
2019-10-04 09:10:54 -0400

    Added picker view

--- W5_Database/W5_Database/ViewControllers/MyPickerViewController.swift
+++ W5_Database/W5_Database/ViewControllers/MyPickerViewController.swift
@@ -8,7 +8,37 @@
 
 import UIKit
 
-class MyPickerViewController: UIViewController {
+class MyPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let mainDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    // Component is a key word
+    // This is the number of rows, using the number of people to populate the picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return mainDelegate.people.count
+    }
+    
+    // This is the number of components in the picker row
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Content to put into the picker row
+    // Row number is passed as INT
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return mainDelegate.people[row].name
+    }
+    
+    // Not common to do, used to fire a specific row
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        let alertController = UIAlertController(title: mainDelegate.people[row].email, message: mainDelegate.people[row].food, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
